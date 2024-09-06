9cae428ad66737229b9e6123897de33969c39b13
Saifulislamshovon <mdsaifulislamshovon@gmail.com>
2024-09-06 21:41:47 +0600

    first commit

---
+++ javascript/operation.js
@@ -0,0 +1,19 @@
+/* var applePrice = 200;
+var orangePrice = 300;
+ var price = (applePrice + orangePrice );
+
+ console.log(applePrice+orangePrice);
+ console.log(typeof price); 
+
+console.log(5>10); */
+
+var peyaraPrice = 40;
+var applePrice = 250;
+
+console.log(peyaraPrice<=applePrice);
+
+
+
+
+
+
