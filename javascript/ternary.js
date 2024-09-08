9173656a02db6e9f672dd10f1567abc9ed87d3f1
Saifulislamshovon <mdsaifulislamshovon@gmail.com>
2024-09-08 20:09:14 +0600

    ternays commit

---
+++ javascript/ternary.js
@@ -0,0 +1,51 @@
+const money = 200;
+
+// if(money>300){
+//     console.log('Bro you are rich!');
+// }
+// else {
+//     if(money <0){
+//         console.log('dont call me today');
+//     }
+
+//     else {
+//         if(money>100){
+//             console.log('Lets hangout today broh');
+//         }
+//     }
+// }
+
+// money<300? console.log('dont call me today') : (console.log('Lets hangout today broh'): console.log('Taile Thak chai khai'));
+
+let price = 900;
+const isLeader= true;
+const discount= price/2 ;
+
+// if(isLeader == true){
+//     price=0;
+// }
+// else{
+//     price = price+ 100;
+// }
+// console.log(price)
+
+// (price>=1000 && isLeader === false) ? console.log(price= price/2) :  console.log(price+ 500) ;
+
+// if(isLeader == true){
+//     if(price>=1000)
+//     price= price + 200;
+// else{
+//     price= price;
+// }
+// }
+// else{
+//    price = price - discount;
+//  }
+
+// console.log(price)
+
+
+
+// Complex Ternary Operator ------
+(isLeader === true) && (price>=1000) ? console.log(price= price + 200) : console.log(price = price - ) ;
+
